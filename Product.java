fcfa8cd3e308bc48382530fd499e61962e7d7e80
mahmudur90 <mahmudurrahmanrifat017@gmail.com>
2024-03-14 13:55:00 +0600

    Initial commit

---
+++ Product.java
@@ -0,0 +1,11 @@
+package Practice;
+public class Product {
+
+    public static void main(String[] args) {
+        System.out.println("Id: 101");
+        System.out.println("Title: iphone 15");
+        System.out.println("Price: 1895 euros");
+        System.out.println("description: perfect product with best image quality");
+        System.out.println("category: phone");
+    }
+}
